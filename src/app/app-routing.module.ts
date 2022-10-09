f45f9fba89ae00506e1a1e813b6af0c1c4224bb5
Antoine <laguetteantoine@gmail.com>
2022-10-09 16:01:32 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RestaurantComponent} from "./pages/restaurant/restaurant.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'restaurants', pathMatch: "full"
+  },
+  {
+    path: 'restaurants', component: RestaurantComponent
+  },
+  {
+    path: 'restaurants/:id', component: RestaurantComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { RestaurantTableComponent } from './components/restaurant-table/restaurant-table.component';
+import { RestaurantFormComponent } from './components/restaurant-form/restaurant-form.component';
+import { RestaurantComponent } from './pages/restaurant/restaurant.component';
+import {FormsModule} from "@angular/forms";
+import {RestaurantService} from "./services/restaurant.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantTableComponent,
+    RestaurantFormComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
